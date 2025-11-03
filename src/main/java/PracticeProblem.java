a71478076e2d01b9f64a29b93a72a2ffa4352e63
akash-kobal-ics4u1-01-25 <akash.kobal@student.tdsb.on.ca>
2025-11-03 13:35:04 -0500

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,32 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
-
-	
+//Question 1
+static int fib(int num) {
+	int f[] = new int[num + 2];
+	int i;
+	f[0] = 0;
+	f[1] = 1;
+	for (i = 2; i <= num; i++) {
+		f[i] = f[i - 1] + f[i - 2];
+	}
+	return f[num];
 }
+//Question 2
+static int minCostClimbingStairs(int[] cost) {
+    if (cost == null || cost.length == 0) return 0;
+        int n = cost.length;
+        if (n == 1) return cost[0];
+        int prev2 = cost[0];
+        int prev1 = cost[1];
+        for (int i = 2; i < n; i++) {
+            int cur = cost[i] + Math.min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        return Math.min(prev1, prev2);
+    }
+}
+	
+
